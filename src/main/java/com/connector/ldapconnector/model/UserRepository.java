7345417ce3f6cbf5370d8d6d2285e6189c80d87f
otsogbadrakhchinzorig <tsogbadrakh@globalsign.com>
2018-07-18 13:41:16 +0900

    Temporarily add idea and target folder to save some time

---
+++ src/main/java/com/connector/ldapconnector/model/UserRepository.java
@@ -0,0 +1,17 @@
+package com.connector.ldapconnector.model;
+
+import org.springframework.data.ldap.repository.LdapRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends LdapRepository<User> {
+
+    User findByUsername(String username);
+
+    User findByUsernameAndPassword(String username, String password);
+
+    List<User> findByUsernameLikeIgnoreCase(String username);
+
+}
