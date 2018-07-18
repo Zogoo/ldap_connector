9cae4f682ffec42120fa7f1c212069aacf131791
otsogbadrakhchinzorig <tsogbadrakh@globalsign.com>
2018-07-18 15:56:06 +0900

    Rename test property to app*-test.yml

--- src/test/java/com/connector/ldapconnector/client/LdapClientFeatureTest.java
+++ src/test/java/com/connector/ldapconnector/client/LdapClientFeatureTest.java
@@ -7,14 +7,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@TestPropertySource(locations="classpath:test.yml")
+@TestPropertySource(locations="classpath:application-test.yml")
 public class LdapClientFeatureTest {
 
     String User = "Administrator";
